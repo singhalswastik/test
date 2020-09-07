317ed189f2b4894a2d634191f9213c305cfba2c2
Swastik Singhal <swastik@iitk.ac.in>
2020-09-07 11:23:49 +0530

    database integration 3

--- database.js
+++ database.js
@@ -4,47 +4,21 @@ const uri =
 const client = new MongoClient(uri);
 
 const addone = (newItem) => {
-  return Promise.resolve()
-    .then(() =>
-      client.db("testDb").collection("testCollection").insertOne(newItem)
-    )
-    .then(
-      (res) => `New listing created with the following id: ${res.insertedId}`
-    )
-    .catch((res) => `Error: ${res}`);
+  return client.db("testDb").collection("testCollection").insertOne(newItem);
 };
 const findone = (query) => {
-  return Promise.resolve()
-    .then(() =>
-      client.db("testDb").collection("testCollection").findOne(query)
-    )
-    .catch((res) => `Error: ${res}`);
+  return client.db("testDb").collection("testCollection").findOne(query);
 };
-const update = (id, newItem,upsertValue) => {
-  return Promise.resolve()
-    .then(() =>
-      client
-        .db("testDb")
-        .collection("testCollection")
-        .updateOne({ id: id }, { $set: newItem }, {$upsert: true})
-    )
-    .then(() => `One item updated`)
-    .catch((res) => `Error: ${res}`);
+const update = (id, newItem, upsertValue) => {
+  return client.db("testDb").collection("testCollection").updateOne({ id: id }, { $set: newItem }, { $upsert: true });
 };
 
 const del = (id) => {
-  return Promise.resolve()
-    .then(() =>
-      client.db("testDb").collection("testCollection").deleteOne({ id: id })
-    )
-    // .then(() => `One item deleted`)
-    .catch((res) => `Error: ${res}`);
+  return client.db("testDb").collection("testCollection").deleteOne({ id: id });
 };
 
 const connect = () => {
-  return Promise.resolve()
-    .then(() => client.connect())
-    .catch(() => "Error connecting to database");
+  return client.connect();
 };
 
 exports.connect = connect;
--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 const database = require("./database");
 
-database.connect().then(() => console.log("Connected to database"));
+database
+  .connect()
+  .then(() => console.log("Connected to database"))
+  .catch((res) => console.log(`Error: ${res}`));
 
 const urlLogger = (request, response, next) => {
   console.log("Request URL:", request.url);
@@ -20,32 +23,57 @@ app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
   console.log(request.query);
-  database.findone(request.query).then((res) => console.log(res));
-  response.send("Got a GET request");
+  database
+    .findone(request.query)
+    .then((res) => {
+      console.log(res);
+      response.send({ request: "GET", res });
+    })
+    .catch((res) => console.log(`Error: ${res}`));
 });
 
-app.post("/", function (request, res) {
+app.post("/", function (request, response) {
   console.log(request.body);
-  database.addone(request.body).then((res) => console.log(res));
-  res.send("Got a POST request");
+  database
+    .addone(request.body)
+    .then((res) => {
+      console.log(res);
+      response.send({ request: "POST", res });
+    })
+    .catch((res) => console.log(`Error: ${res}`));
 });
 
-app.patch("/:id", function (request, res) {
+app.patch("/:id", function (request, response) {
   console.log(request.body);
-  database.update(request.params.id,request.body).then((res) => console.log(res));
-  res.send("Got a PATCH request");
+  database
+    .update(request.params.id, request.body)
+    .then((res) => {
+      console.log(res);
+      response.send({ request: "PATCH", res });
+    })
+    .catch((res) => console.log(`Error: ${res}`));
 });
 
-app.put("/:id", function (request, res) {
+app.put("/:id", function (request, response) {
   console.log(request.body);
-  database.update(request.params.id,request.body).then((res) => console.log(res));
-  res.send("Got a PUT request");
+  database
+    .update(request.params.id, request.body)
+    .then((res) => {
+      console.log(res);
+      response.send({ request: "PUT", res });
+    })
+    .catch((res) => console.log(`Error: ${res}`));
 });
 
-app.delete("/:id", function (request, res) {
+app.delete("/:id", function (request, response) {
   console.log(request.body);
-  database.delete(request.params.id).then((res) => console.log(res));
-  res.send("Got a DELETE request");
+  database
+    .delete(request.params.id)
+    .then((res) => {
+      console.log(res);
+      response.send({ request: "DELETE", res });
+    })
+    .catch((res) => console.log(`Error: ${res}`));
 });
 
 app.listen(3000, () => {
